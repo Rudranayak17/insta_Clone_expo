7404bb382ee5452845c55a74c2b6e76473677fd0
Rudranayak17 <122798243+Rudranayak17@users.noreply.github.com>
2024-05-24 22:31:34 +0530

    update the tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,26 +1,64 @@
 import Colors from "@/constants/Colors";
-import { AntDesign, Entypo, FontAwesome, Ionicons } from "@expo/vector-icons";
+import {
+  AntDesign,
+  Entypo,
+  FontAwesome,
+  FontAwesome5,
+  Ionicons,
+} from "@expo/vector-icons";
 import { Tabs, useSegments } from "expo-router";
 
 import reelsIcon from "@/assets/images/ui/instagram-reels-icon.png";
-import { Image } from "react-native";
-
+import { Image, TouchableOpacity, View } from "react-native";
+import InstaIcon from "@/assets/images/ui/instagram-text-icon.png";
 const TabsLayout = () => {
-
-
   return (
     <Tabs
       screenOptions={{
         tabBarShowLabel: false,
-        headerShown: false,
+      
         tabBarHideOnKeyboard: true,
-
       }}
-    
     >
       <Tabs.Screen
         name="home/index"
         options={{
+          headerTitle(props) {
+            return (
+              <Image
+                source={InstaIcon}
+                style={{ width: 90, height: 50, resizeMode: "contain" }}
+              />
+            );
+          },
+          headerRight(props) {
+            return (
+              <View
+                style={{
+                  paddingHorizontal: 10,
+                  flexDirection: "row",
+                  gap: 20,
+                }}
+              >
+                <TouchableOpacity onPress={()=>console.log("fav")}>
+                  <AntDesign name="hearto" size={24} color="black" />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=>console.log("message")}>
+                  <FontAwesome5
+                    name="facebook-messenger"
+                    size={24}
+                    color="black"
+                  />
+                </TouchableOpacity>
+              </View>
+            );
+          },
+          // headerLeft: () => {
+          //   return <Image
+          //     source={InstaIcon}
+          //     style={{ width: 200, height: 200, resizeMode: "contain" }}
+          //   />;
+          // },
           tabBarIcon: ({ size, color }) => (
             <Entypo name="home" size={24} color="black" />
           ),
@@ -29,15 +67,16 @@ const TabsLayout = () => {
       <Tabs.Screen
         name="search/index"
         options={{
+          headerShown: false,
           tabBarIcon: ({ size, color }) => (
             <AntDesign name="search1" size={24} color="black" />
           ),
-          headerShown: false,
         }}
       />
       <Tabs.Screen
         name="upload/index"
         options={{
+          headerShown: false,
           tabBarIcon: ({ size, color }) => (
             <Entypo name="squared-plus" size={24} color="black" />
           ),
@@ -46,7 +85,7 @@ const TabsLayout = () => {
       <Tabs.Screen
         name="reels/index"
         options={{
-          title: "Chats",
+          headerShown: false,
           tabBarIcon: ({ size, color }) => (
             <Image
               source={reelsIcon}
@@ -63,14 +102,12 @@ const TabsLayout = () => {
       <Tabs.Screen
         name="profile/index"
         options={{
-          title: "Settings",
+          headerShown: false,
           tabBarIcon: ({ size, color }) => (
             <FontAwesome name="user-circle" size={24} color="black" />
           ),
-          headerShown: false,
         }}
       />
-    
     </Tabs>
   );
 };
